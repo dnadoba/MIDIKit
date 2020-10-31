12031f53073b8dc71a3e4c023aea66f50452deb5
Michele Longhi <michele.longhi@users.noreply.github.com>
2020-10-31 15:44:47 +0100

    #if os(iOS) check in MIDINetworkClient

--- Sources/MIDIKit/MIDINetworkClient.swift
+++ Sources/MIDIKit/MIDINetworkClient.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreMIDI
 
-@available(iOS 4.2, *)
+#if os(iOS)
 public class MIDINetworkClient: MIDIClient {
     
     public private(set) var networkSession: MIDINetworkSession?
@@ -69,3 +69,4 @@ public class MIDINetworkClient: MIDIClient {
         removeAllConnections()
     }
 }
+#endif
